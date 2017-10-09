ea9ac8548aa811b745a014e2774227d5802c63c2
Unknown <e1c17086@oit.ac.jp>
2017-10-10 01:44:30 +0900

    item-shotadd 2way,3way

--- Assets/Scripts/Bulletcontroller.cs
+++ Assets/Scripts/Bulletcontroller.cs
@@ -3,42 +3,87 @@ using System.Collections;
 
 public class Bulletcontroller : MonoBehaviour
 {
-    public int power = 1;
-    void Update()
-    {
-        transform.Translate(0.2f, 0, 0);
+	public int power = 1;
+	public int way = 1;
 
-        if (transform.position.x > 10)
-        {
-            Destroy(gameObject);
-        }
-    }
-    void OnTriggerEnter2D(Collider2D collision)
-    {
+	void Update ()
+	{
+		/*transform.Translate(0.2f, 0, 0);
+
+		if (transform.position.x > 10)
+		{
+			Destroy(gameObject);
+		}*/
+		addshot ();
+
+		if (transform.position.x > 10) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D collision)
+	{
        
 
-        // 衝突したときにスコアを更新する
+		// 衝突したときにスコアを更新する
         
 
-        string layerName = LayerMask.LayerToName(collision.gameObject.layer);
-        // Debug.Log(layerName);
-        // レイヤー名がBullet (Player)以外の時は何も行わない
+		string layerName = LayerMask.LayerToName (collision.gameObject.layer);
+		// Debug.Log(layerName);
+		// レイヤー名がBullet (Player)以外の時は何も行わない
         
-            if (layerName == "Enemy") {
-         //   Debug.Log("とおって");
+		if (layerName == "Enemy") {
+			//   Debug.Log("とおって");
 
-            //  GameObject.Find("Canvas").GetComponent<UIcontroller>().AddScore();
-        }
-            if (layerName == "Player")
-        {
+			//  GameObject.Find("Canvas").GetComponent<UIcontroller>().AddScore();
+		}
+		if (layerName == "Player") {
 
             
-            //aitewokesu
-            Destroy(collision.gameObject);
-           // Debug.Log("とおって");
-            Destroy(gameObject);
+			//aitewokesu
+			Destroy (collision.gameObject);
+			// Debug.Log("とおって");
+			Destroy (gameObject);
+		}
+	}
+
+
+
+	public void addshot (/*int shot*/)
+	{
+		// 子要素を全て取得する
+		/*int i=0;
+        if (i < transform.childCount)
+        {
+
+
+
+            i++;
         }
-    }
+
+        {
+
+            Transform shotPosition = transform.GetChild(i);
+
+            // ShotPositionの位置/角度で弾を撃つ
+            Shot(shotPosition);
+            }*/
+
+		if (transform.name == "BulletPrefab1") {
+			transform.Translate (0.2f, 0, 0);
+
+		}
+		if (transform.name == "BulletPrefab2") {
+			transform.Translate (0.2f, 0.1f, 0);
+
+		}
+		if (transform.name == "BulletPrefab3") {
+			transform.Translate (0.2f, -0.1f, 0);
+
+		}
+	}
+
 
 
 }
+
--- Assets/Scripts/Itemscript.cs
+++ Assets/Scripts/Itemscript.cs
@@ -7,24 +7,30 @@ public class Itemscript : MonoBehaviour
     
 	public int shot = 1;
 	public Player playerScript;
-	GameObject dropobj;
+	GameObject shotaddTxc;
 
 
   
 	void OnCollisionEnter2D (Collision2D collision)
 	{
 		if (collision.gameObject.tag == "character 2") {//shotaddメソッドを呼び出す　引数はshotpoint
-        
-			playerScript.addshot (shot);
-			Destroy (gameObject);
+			if (gameObject == shotaddTxc) {
+				FindObjectOfType<Player> ().wayadd ();
+			}
 		}
+	}
+
+	private void addshotcall ()
+	{
+		//FindObjectOfType<Bulletcontroller> ().addshot ();
+
 	}
 
 	public void itemdrop ()
 	{
 
 		if (Random.Range (0, 49) == 0) {
-			Instantiate (dropobj, transform.position, transform.rotation);
+			Instantiate (shotaddTxc, transform.position, transform.rotation);
 		}
 	}
 
